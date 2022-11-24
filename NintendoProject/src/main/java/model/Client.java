fac938b3fae67b0a33d34538cf3dba367b01e720
manonbonnin <manonbonnin@yahoo.fr>
2022-11-24 15:08:24 +0100

    Achat sur client - MB

--- NintendoProject/src/main/java/model/Client.java
+++ NintendoProject/src/main/java/model/Client.java
@@ -1,27 +1,61 @@
 package model;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
 public class Client {
 private String nom;
 private String prenom;
-public Client(String nom, String prenom) {
+
+
+private List<Achat> achat1 = new ArrayList();
+
+
+
+public Client(String nom, String prenom, List<Achat> achat1) {
+	super();
 	this.nom = nom;
 	this.prenom = prenom;
+	this.achat1 = achat1;
 }
+
+
 public String getNom() {
 	return nom;
 }
-public String getPrenom() {
-	return prenom;
-}
+
+
 public void setNom(String nom) {
 	this.nom = nom;
 }
+
+
+public String getPrenom() {
+	return prenom;
+}
+
+
 public void setPrenom(String prenom) {
 	this.prenom = prenom;
 }
+
+
+public List<Achat> getAchat1() {
+	return achat1;
+}
+
+
+public void setAchat1(List<Achat> achat1) {
+	this.achat1 = achat1;
+}
+
+
 @Override
 public String toString() {
-	return "Client [nom=" + nom + ", prenom=" + prenom + "]";
+	return "Client [nom=" + nom + ", prenom=" + prenom + ", achat1=" + achat1 + "]";
 }
+
+
 
 }
--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -2,7 +2,11 @@ package test;
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import model.Achat;
 import model.Adresse;
 import model.Boutique;
 import model.Client;
@@ -13,8 +17,12 @@ import model.Jeu;
 
 
 public class Test {
+	
+	static List<Achat> achat1 = new ArrayList();
 
 	public static void main(String[] args) {
+		
+		
 		
 		Console c1 = new Console("PES",25, LocalDate.parse("2016-05-01"));
 		
@@ -24,8 +32,13 @@ public class Test {
 		Jeu j3 = new Jeu("puzzle", c1);
 		Jeu j4 = new Jeu("subway", c1);
 		
-		Client cl1 = new Client("Robert","Dupont");
-		Client cl2 = new Client("Bernadette","Dupont");
+		Achat a1 = new Achat(j,LocalDate.now(),30.5);
+		Achat a2 = new Achat(j1,LocalDate.now(),40.5);
+		
+		Collections.addAll(achat1,a1,a2);
+		
+		Client cl1 = new Client("Robert","Dupont",achat1);
+		Client cl2 = new Client("Bernadette","Dupont",achat1);
 		
 		Adresse a = new Adresse (5,"Hugo", "Paris") ;
 		Boutique b = new Boutique("Lidl", a);
