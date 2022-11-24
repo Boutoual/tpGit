578be5965f91e629a6c53ed12d169ad965594f96
Khamad Taher <attaherikhiri@yahoo.fr>
2022-11-24 13:22:37 +0100

    Maj boutique -> Adresse

--- NintendoProject/src/main/java/model/Boutique.java
+++ NintendoProject/src/main/java/model/Boutique.java
@@ -3,25 +3,24 @@ package model;
 public class Boutique {
 	
 	String nom;
-	String adresse;
+	Adresse adresse;
 	public String getNom() {
 		return nom;
 	}
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	public String getAdresse() {
+	public Adresse getAdresse() {
 		return adresse;
 	}
-	public void setAdresse(String adresse) {
+	public void setAdresse(Adresse adresse) {
 		this.adresse = adresse;
 	}
 	@Override
 	public String toString() {
 		return "Boutique [nom=" + nom + ", adresse=" + adresse + "]";
 	}
-	public Boutique(String nom, String adresse) {
-		super();
+	public Boutique(String nom, Adresse adresse) {
 		this.nom = nom;
 		this.adresse = adresse;
 	}
