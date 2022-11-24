f49b9e481f55015ca8748aab4dab5bbf37097c13
manonbonnin <manonbonnin@yahoo.fr>
2022-11-24 12:37:36 +0100

    classe boutique - MB

---
+++ NintendoProject/src/main/java/model/Boutique.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Boutique {
+	
+	String nom;
+	String adresse;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + ", adresse=" + adresse + "]";
+	}
+	public Boutique(String nom, String adresse) {
+		super();
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+	
+	
+
+}
